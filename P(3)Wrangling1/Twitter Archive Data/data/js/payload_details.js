f4d4373bf4f1614e40009351fb571562a8976816
Beltino Goncalves <goncalbj@clarkson.edu>
2018-07-22 11:48:18 -0400

    Add files via upload

---
+++ P(3)Wrangling1/Twitter Archive Data/data/js/payload_details.js
@@ -0,0 +1,5 @@
+var payload_details =  {
+  "tweets" : 0,
+  "created_at" : "2018-01-04 16:20:21 +0000",
+  "lang" : "en"
+}
---
+++ P(3)Wrangling1/Twitter Archive Data/data/js/user_details.js
@@ -0,0 +1,6 @@
+var user_details =  {
+  "screen_name" : "Beltacoq",
+  "created_at" : "2018-01-04 15:51:33 +0000",
+  "full_name" : "Beltaco",
+  "id" : "948944999563096065"
+}
